824e0dadd8c096dc516d2df0cf8ae6bb1e8ad42e
Tên GitHub của bạn <email-github@example.com>
2026-09-02 23:03:21 +0700

    sửa module CLK gen

--- src/CLK_gen.v
+++ src/CLK_gen.v
@@ -1,31 +1,46 @@
-module CLK_gen (
-    parameter CLK_1s = 50_000_000,
-    parameter CLK_1p = 3_000_000_000
+module CLK_gen #(
+    parameter [31:0] CLK_1s = 50_000_000,
+    parameter [31:0] CLK_1p = 3_000_000_000
 ) (
-    input clk,
+    input wire clk,
+    input wire rst_n,
 
-    output tick_1s,
-    output tick_1p
+    output reg tick_1s,
+    output reg tick_1p
 );
-    reg [31:0] tick_1s_temp;
-    reg [31:0] tick_1p_temp;
+    reg [31:0] tick_1s_count;
+    reg [31:0] tick_1p_count;
 
-    assign tick_1s_temp = 32'd0;
-    assign tick_1p_temp = 32'd0;
-
-    always @(posedge clk) begin
-        tick_1s_temp <= tick_1s_temp + 32'd1;
-        tick_1p_temp <= tick_1p_temp + 32'd1;
-
-        if (tick_1s_temp) begin
-            tick_1s <= 1'b1;
-        end
-        else if (tick_1p_temp) begin
-            tick_1p <= 1'b1;
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            tick_1s <= 1'd0;
+            tick_1p <= 1'd0;
+            tick_1p_count <= 32'd0;
+            tick_1s_count <= 32'd0;
         end
         else begin
             tick_1s <= 1'b0;
             tick_1p <= 1'b0;
+
+            tick_1s_count <= tick_1s_count + 32'd1;
+            tick_1p_count <= tick_1p_count + 32'd1;
+
+            if (tick_1s_count == CLK_1s - 1'd1) begin
+                tick_1s_count <= 32'd0;
+                tick_1s <= 1'b1;
+            end
+            else begin
+                tick_1s <= 1'b0;
+            end
+
+            if (tick_1p_count == CLK_1p - 1'd1) begin
+                tick_1p_count <= 32'd0;
+                tick_1p <= 1'b1;
+            end
+            else begin
+                tick_1p <= 1'b0;
+            end     
         end
     end
+    
 endmodule
